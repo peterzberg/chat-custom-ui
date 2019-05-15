fca48838e251d529b98291d8691021af21e9fb8a
p15843 <peter.zberg@css.ch>
2019-05-15 09:38:07 +0200

    added cors (globally)

--- src/main/java/com/zberg/sample/chatbot/api/ConversationController.java
+++ src/main/java/com/zberg/sample/chatbot/api/ConversationController.java
@@ -7,14 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.UUID;
 
+@CrossOrigin
 @RestController("/api/conversation")
 public class ConversationController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ConversationController.class);
